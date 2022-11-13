8cc3866960c28625934297bfeb265518f1207d0c
hoanganhfuego <hoanganhjalai12@gmail.com>
2022-11-13 22:08:47 +0700

    add new test

--- js.js
+++ js.js
@@ -1,16 +1,14 @@
 const testGit = () => {
-    console.log("test 1")
-}
+  console.log("test 1");
+};
 
 const testGit2 = () => {
-    console.log("test 2")
-}
+  console.log("test 2");
+};
 
-<<<<<<< HEAD
 const testGit4 = () => {
-    console.log("test 4")
-=======
+  console.log("test 4");
+};
 const testGit3 = () => {
-    console.log("test 3")
->>>>>>> ec31e24 (test 3)
-}
+  console.log("test 3");
+};
