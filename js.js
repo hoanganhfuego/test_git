5f2d386d6a8ec69ab053da26ad2cbb26df25cbda
hoanganhfuego <hoanganhjalai12@gmail.com>
2022-11-13 21:50:04 +0700

    test 3

--- js.js
+++ js.js
@@ -6,6 +6,11 @@ const testGit2 = () => {
     console.log("test 2")
 }
 
+<<<<<<< HEAD
 const testGit4 = () => {
     console.log("test 4")
+=======
+const testGit3 = () => {
+    console.log("test 3")
+>>>>>>> ec31e24 (test 3)
 }
