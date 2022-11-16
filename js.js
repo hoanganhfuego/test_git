c120ebf5cb228835e5d2a9fcf7c5a940e57f1ae0
hoanganhfuego <hoanganhjalai12@gmail.com>
2022-11-16 21:05:01 +0700

    rebase state

--- js.js
+++ js.js
@@ -1,5 +1,4 @@
 const testGit = () => {
-<<<<<<< HEAD
   console.log("test 1");
 };
 
@@ -13,9 +12,5 @@ const testGit4 = () => {
 const testGit3 = () => {
   console.log("test 3");
 };
-=======
-    console.log("test 1")
-}
 
 console.log("xin chao toi la rebase")
->>>>>>> 4a1fe81 (test rebase branch)
